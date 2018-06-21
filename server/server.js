d7663d2df380f23383f978760697f116ac841c4b
Joao Manuel <joaocsmanuel@gmail.com>
2018-06-21 12:24:55 +0100

    Add GET and POST

--- server/server.js
+++ server/server.js
@@ -1,24 +1,76 @@
-var mongoose = require('mongoose');
+const { ObjectID } = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser'); // takes json and convert it into a object
 
-mongoose.connect('mongodb://localhost:27017/H2hTestApp');
+const { mongoose } = require('./db/mongoose');
+const { Idea } = require('./models/idea');
+const { User } = require('./models/user');
 
-// a mongoose model
-var Idea = mongoose.model('Idea', {
-  title: {
-    type: String
-  },
-  description: {
-    type: String
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/ideas', (req, res) => {
+  const idea = new Idea({
+    title: req.body.title,
+    description: req.body.description
+  });
+
+  idea.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+})
+
+app.get('/ideas', (req, res) => {
+  Idea.find().then((ideas) => {
+    res.send({ ideas });
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/ideas/:id', (req, res) => {
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
   }
+
+  Idea.findById(id).then((idea) => {
+    if(!idea){
+     return res.status(404).send();      
+    }
+
+    res.send({ idea });    
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-var newIdea = new Idea({
-  title: 'New idea',
-  description: 'some description'
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
 
-newIdea.save().then((doc) => {
-  console.log('Saved idea', doc);
-}, (e) => {
-  console.log('Unable to save idea')
-});
+module.exports = { app };
+
+// var newIdea = new Idea({
+//   title: '',
+//   description: 'another description'
+// });
+
+//
+
+
+// var newUser = new User({
+//   firstName: 'João',
+//   lastName: 'Manuel',
+//   email: 'joaocsmanuel@gmail.com'
+// });
+
+// newUser.save().then((doc) => {
+//   console.log('Saved user', doc);
+// }, (e) => {
+//   console.log('Unable to save user')
+// });
