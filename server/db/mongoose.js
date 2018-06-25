0b3893a999a8b6897690b19ca5bd15b365ad084b
Joao Manuel <joaocsmanuel@gmail.com>
2018-06-25 17:50:20 +0100

    Port changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,6 @@ let db = {
   mlab: 'mongodb://h2htestadmin:h2htestadmin@ds016118.mlab.com:16118/h2htestapp'
 };
 
-mongoose.connect(mongoose.connect( process.env.PORT ? db.mlab : db.localhost));
+mongoose.connect(process.env.MONGODB_URI || db.localhost);
 
 module.exports = { mongoose };
